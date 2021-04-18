038e22437f81314c163bae1090df4dca7c91a635
ptrickd <patrickdion8@gmail.com>
2021-04-18 12:58:39 -0600

    js exercise 2 done

---
+++ javascript/two-fer/two-fer.js
@@ -0,0 +1,11 @@
+//
+// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const twoFer = (name) => {
+  let value = 'you'
+  if (name) value = name
+  return `One for ${value}, one for me.`
+
+};
