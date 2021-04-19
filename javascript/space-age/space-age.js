91ea4e446ddcbe2452ada46310b8b7acfb5eee1c
ptrickd <patrickdion8@gmail.com>
2021-04-19 08:59:05 -0600

    javascript/space-age

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -3,6 +3,19 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this statement and implement this function');
+export const age = (planet, ageInSeconds) => {
+  const yearEarthInSecond = 31557600
+  const ageFactored = ageInSeconds / ageConstant[planet] / yearEarthInSecond
+  return Number(ageFactored.toFixed(2))
 };
+
+const ageConstant = {
+  'mercury': 0.2408467,
+  'venus': 0.61519726,
+  'earth': 1.0,
+  'mars': 1.8808158,
+  'jupiter': 11.862615,
+  'saturn': 29.447498,
+  'uranus': 84.016846,
+  'neptune': 164.79132
+}
